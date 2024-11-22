9764b3e9560d6bdaeade9e1d6ef7022295920331
AALIYAN540 <aaliyanarshad4@gmail.com>
2024-11-22 16:29:42 +0500

    Cpp Code
    Academy Practice Code

---
+++ DEC.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int a,b,c,d;
+    a=b=c=d=2;
+    a=--c;
+    b=d--;
+    cout<<"a="<<a<<endl;
+     cout<<"b="<<b<<endl;
+      cout<<"c="<<c<<endl;
+       cout<<"d="<<d<<endl;
+       return 0;
+}
---
+++ compound.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int x, y;
+
+    cout<<"Enter your values:\n";
+    cin>>x >>y;
+
+    cout<<"Sum = "<<x+y<<endl;
+    cout<<"Sub ="<<x-y<<endl;
+    cout<<"Mul ="<<x*y<<endl;
+    cout<<"Div ="<<x/y<<endl;
+    cout<<"Mod ="<<x%y<<endl;
+    
+    return 0;
+}
